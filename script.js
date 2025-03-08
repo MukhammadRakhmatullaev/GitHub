dad0897017e0def37d104b44f3be0ac374dafd5a
MukhammadRakhmatullaev <mukhammadrakhmatullaev765@gmail.com>
2025-03-09 04:10:24 +0500

    Finish

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
-/* Please ❤ this if you like it! */
-
-
 (function($) { "use strict";
-		
-	//Page cursors
 
-    document.getElementsByTagName("body")[0].addEventListener("mousemove", function(n) {
+document.getElementsByTagName("body")[0].addEventListener("mousemove", function(n) {
         t.style.left = n.clientX + "px", 
 		t.style.top = n.clientY + "px", 
 		e.style.left = n.clientX + "px", 
 		e.style.top = n.clientY + "px", 
 		i.style.left = n.clientX + "px", 
 		i.style.top = n.clientY + "px"
-    });
+});
     var t = document.getElementById("cursor"),
         e = document.getElementById("cursor2"),
         i = document.getElementById("cursor3");
@@ -29,9 +24,6 @@
     function o(t) {
         t.addEventListener("mouseover", n), t.addEventListener("mouseout", s)
     }
-
-	
-	//About page
 	
 	$(".about-text").on('click', function () {
 		$("body").addClass("about-on");
@@ -39,9 +31,6 @@
 	$(".about-close").on('click', function () {
 		$("body").removeClass("about-on");
 	});
-
-	
-	//Contact page
 	
 	$(".contact-text").on('click', function () {
 		$("body").addClass("contact-on");
@@ -49,9 +38,6 @@
 	$(".contact-close").on('click', function () {
 		$("body").removeClass("contact-on");
 	});
-
-	
-	//Travel portfolio page
 	
 	$(".travel").on('click', function () {
 		$("body").addClass("travel-on");
@@ -59,9 +45,6 @@
 	$(".travel-close").on('click', function () {
 		$("body").removeClass("travel-on");
 	});
-
-	
-	//Wildlife portfolio page
 	
 	$(".wildlife").on('click', function () {
 		$("body").addClass("wildlife-on");
@@ -69,9 +52,6 @@
 	$(".wildlife-close").on('click', function () {
 		$("body").removeClass("wildlife-on");
 	});
-
-	
-	//Nature portfolio page
 	
 	$(".nature").on('click', function () {
 		$("body").addClass("nature-on");
@@ -79,6 +59,5 @@
 	$(".nature-close").on('click', function () {
 		$("body").removeClass("nature-on");
 	});
-
 	
 })(jQuery);
